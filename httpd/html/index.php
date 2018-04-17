7033a6d000ace7a2ab47b5d7345cfa16608afa84
Choon Keat <cseet@localhost.localdomain>
2018-04-18 01:00:36 +0800

    Make for httpd configurations and httpd content files Pending virtual hosts and switch to different port 	new file:   httpd/Makefile 	new file:   httpd/html/index.php

---
+++ httpd/html/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+	</head>
+	<body>
+
+		<h1>Applications</h1> 
+		<ul>
+			<li><a href="/javadoc/docs/api/index.html?overview-summary.html">Java 9 Documentation</a></li>
+		</ul>
+
+		<?php phpinfo();?>
+
+	</body>
+<html>
